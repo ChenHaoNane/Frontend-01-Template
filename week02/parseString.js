72c284d07c16b6012bd13c4977c5c56becb6e898
Haonan Chen <chenhaonan_2008@163.com>
2020-04-22 23:48:10 +0800

    feat(core): complete

---
+++ week02/parseString.js
@@ -0,0 +1,13 @@
+// 暂未完成
+// function parseString(str) {
+//   if (!str) return null
+//   var reg = /^"[^"\\]"$/
+
+//   var b = /((\\x[0-9a-f]{2})|(\\u[0-9a-f]{4})|[^\\"]|\\['"\\])*/
+//   LF = /\\u000A/
+//   CR = /\\u000D/
+//   LS = 
+//   var 
+
+// }
+
