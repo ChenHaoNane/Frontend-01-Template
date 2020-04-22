432c4deeaf7339306eb6b5a9134ec77f2982ca32
Haonan Chen <chenhaonan_2008@163.com>
2020-04-23 00:25:05 +0800

    feat(core): complete parseString

--- week02/parseString.js
+++ week02/parseString.js
@@ -1,13 +1,7 @@
-// 暂未完成
-// function parseString(str) {
-//   if (!str) return null
-//   var reg = /^"[^"\\]"$/
-
-//   var b = /((\\x[0-9a-f]{2})|(\\u[0-9a-f]{4})|[^\\"]|\\['"\\])*/
-//   LF = /\\u000A/
-//   CR = /\\u000D/
-//   LS = 
-//   var 
-
-// }
+function parseString(str) {
+  if (!str) return null
+  let reg = /(("([^"\r\n\u2028\u2029\\]|\u2028|\u2029|\\((['"\bfnrtv]|[^'"\bfnrtvdxu\r\n\u2028\u2029])|0(?!d)|x[0-9a-fA-F][0-9a-fA-F]|(u[0-9a-fA-F]{4}|u{(0[0-9a-fA-F]{5}|10[0-9a-fA-F]{4}|[0-9a-fA-F]{1,4})}))|\\(\r\n|[\r\n\u2028\u2029]))*"|'([^'\r\n\u2028\u2029\\]|\u2028|\u2029|\\((['"\bfnrtv]|[^'"\bfnrtvdxu\r\n\u2028\u2029])|0(?!d)|x[0-9a-fA-F][0-9a-fA-F]|(u[0-9a-fA-F]{4}|u{(0[0-9a-fA-F]{5}|10[0-9a-fA-F]{4}|[0-9a-fA-F]{1,4})}))|\\(\r\n|[\r\n\u2028\u2029]))*'))/    
+  let match = str.match(reg)
+  return match && match[0]
+}
 
