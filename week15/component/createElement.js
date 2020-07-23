938939fcb3e54731d1bdf42c2cb0f3279f4fc451
Haonan Chen <chenhaonan_2008@163.com>
2020-07-23 23:30:04 +0800

    done

---
+++ week15/component/createElement.js
@@ -0,0 +1,65 @@
+export function createElement(Cls, attributes, ...children) {
+    let o;
+    if (typeof Cls === "string") {
+        o = new Wrapper(Cls)
+    } else {
+        o = new Cls();
+    }
+    
+    for (let name in attributes) {
+        o.setAttribute(name, attributes[name]);
+    }
+
+    let visit = (children) => {
+        for (let child of children) {
+            if (child instanceof Array) {
+                visit(child);
+                continue;
+            }
+            if (typeof child === "string") 
+                child = new Text(child);
+            o.appendChild(child);
+        }
+    }
+    visit(children)
+    return o
+}
+
+export class Wrapper {
+    constructor(type) { // config
+        this.children = []
+        this.root = document.createElement(type)
+    }
+    setAttribute(name, value) { // attribute
+        this.root.setAttribute(name, value)
+    }
+
+    appendChild(child) { // children
+        this.children.push(child)
+    }
+
+    addEventListener() {
+        this.root.addEventListener(...arguments)
+    }
+
+    get style() {
+        return this.root.style
+    }
+
+    mountTo(parent) {
+        parent.appendChild(this.root)
+        for (let child of this.children) {
+            child.mountTo(this.root)
+        }
+    }
+}
+
+export class Text {
+    constructor(text) { // config
+        this.children = []
+        this.root = document.createTextNode(text)
+    }
+    mountTo(parent) {
+        parent.appendChild(this.root)
+    }
+}
